741714f0da099206ed45ac2a0bd2ea2994ec927b
Preethi Korukuri <preethikorukuri10@gmail.com>
2023-07-05 22:01:59 +0530

    End of section8

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
 import reportWebVitals from './reportWebVitals';
-import App from './app/layout/App';
 import { StoreContext, store } from './app/stores/store';
+import { router } from './app/router/Routes';
+import { RouterProvider } from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <StoreContext.Provider value={store}>
-    <App/>
-  </StoreContext.Provider>,
-  document.getElementById('root')
+    <RouterProvider router={router}/>
+  </StoreContext.Provider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
