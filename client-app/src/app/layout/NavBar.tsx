7c3f53e74652f6bad7923b5cdcaa48fb77453321
Preethi Korukuri <preethikorukuri10@gmail.com>
2023-07-11 14:59:47 +0530

    End of Section17

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,29 +1,28 @@
-import { Button, Container, Menu, Image, Dropdown} from 'semantic-ui-react';
-import { Link, NavLink } from 'react-router-dom';
-import { useStore } from '../stores/store';
-import { observer } from 'mobx-react-lite';
+import {Button, Container, Dropdown, Menu, Image} from "semantic-ui-react";
+import { Link, NavLink } from "react-router-dom";
+import { useStore } from "../stores/store";
+import { observer } from "mobx-react-lite";
 
 export default observer(function NavBar() {
-    const {userStore : {user,logout}} = useStore();
-    return(
+    const {userStore: {user, logout}} = useStore();
+    return (
         <Menu inverted fixed='top'>
             <Container>
                 <Menu.Item as={NavLink} to='/' header>
-                    <img src = "/assets/logo.png" alt= "logo" style={{marginRight:'10px'}}/>
+                    <img src='/assets/logo.png' alt='logo' style={{marginRight: 10}}/>
                     Reactivities
                 </Menu.Item>
-                <Menu.Item as={NavLink} to='/activities' name='Activities'/>
-                <Menu.Item as={NavLink} to='/errors' name='Errors'/>
+                <Menu.Item as={NavLink} to='/activities' name='Activities' />
+                <Menu.Item as={NavLink} to='/errors' name='Errors' />
                 <Menu.Item>
-                    <Button as={NavLink} to='/createActivity' positive content='Create Activity'/>
+                    <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
                 </Menu.Item>
                 <Menu.Item position='right'>
-                    <Image src={user?.image || '/assets/user.png'} avatar spaced='right'/>
+                    <Image avatar spaced='right' src={user?.image || '/assets/user.png'} />
                     <Dropdown pointing='top left' text={user?.displayname}>
                         <Dropdown.Menu>
-                            <Dropdown.Item as={Link} to={`/profile/${user?.username}`} 
-                               text='My Profile' icon='user'/>
-                            <Dropdown.Item onClick={logout} text='Logout' icon='power'/>
+                            <Dropdown.Item as={Link} to={`/profiles/${user?.username}`} text='My Profile' icon='user' />
+                            <Dropdown.Item onClick={logout} text='Logout' icon='power' />
                         </Dropdown.Menu>
                     </Dropdown>
                 </Menu.Item>
--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,54 +1,65 @@
 import { makeAutoObservable, runInAction } from "mobx";
-import { User, UserFormValues } from "../models/user";
 import agent from "../api/agent";
-import { store } from "./store";
+import { User, UserFormValues } from "../models/user";
 import { router } from "../router/Routes";
+import { store } from "./store";
 
 export default class UserStore {
-    user : User | null = null;
+    user: User | null = null;
 
     constructor() {
         makeAutoObservable(this)
     }
 
     get isLoggedIn() {
-        return !! this.user;
+        return !!this.user;
     }
-    login = async (creds : UserFormValues) => {
+
+    login = async (creds: UserFormValues) => {
         try {
             const user = await agent.Account.login(creds);
             store.commonStore.setToken(user.token);
-            runInAction(()=>this.user=user);
+            runInAction(() => this.user = user);
             router.navigate('/activities');
             store.modalStore.closeModal();
-            console.log(user);
-        } catch(error) {
+        } catch (error) {
             throw error;
         }
     }
-    register = async (creds : UserFormValues) => {
+
+    register = async (creds: UserFormValues) => {
         try {
             const user = await agent.Account.register(creds);
             store.commonStore.setToken(user.token);
-            runInAction(()=>this.user=user);
+            runInAction(() => this.user = user);
             router.navigate('/activities');
             store.modalStore.closeModal();
-            console.log(user);
-        } catch(error) {
+        } catch (error) {
             throw error;
         }
     }
+
+
     logout = () => {
         store.commonStore.setToken(null);
-        this.user=null;
+        this.user = null;
         router.navigate('/');
     }
-    getUser =  async () => {
+
+    getUser = async () => {
         try {
             const user = await agent.Account.current();
-            runInAction(()=>this.user=user);
+            runInAction(() => this.user = user);
         } catch (error) {
             console.log(error);
         }
+    }
+
+    setImage = (image: string) => {
+        if (this.user) this.user.image = image;
+    }
+
+    setUserPhoto = (url: string) => {
+        if (this.user) this.user.image = url;
     }
 }
